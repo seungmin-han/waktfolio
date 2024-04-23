8cb51aefef668f78f4c1ea7ad765c00aa9f062ae
SEUNGMIN HAN <seungmin_han@kakao.com>
2024-04-23 17:17:15 +0900

    Feat/Add: 로그인 페이지 임시 구현

---
+++ src/views/LoginView.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="w-full h-full flex justify-center items-center">
+        <div class="flex flex-col items-center w-[800px]">
+            <label class="flex justify-between items-center w-full">
+                아이디
+                <input type="text" />
+            </label>
+            <label class="flex justify-between items-center w-full">
+                비밀번호
+                <input type="text" />
+            </label>
+            <label class="flex justify-between items-center w-full">
+                이름
+                <input type="text" />
+            </label>
+            <label class="flex justify-between items-center w-full">
+                이미지 선택
+                <input type="file" accept=".png,.jpg,.jpeg" @change="changeFile" />
+            </label>
+
+            <button @click="register">회원가입</button>
+        </div>
+    </div>
+</template>
+
+<script setup>
+function register() {}
+
+function changeFile($e) {
+    console.log($e.target.files);
+}
+</script>
+
+<style lang="scss" scoped></style>
