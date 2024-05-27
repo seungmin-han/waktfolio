b3a97325214f4cb6767267ff109604ec82d3dc45
SEUNGMIN HAN <seungmin_han@kakao.com>
2024-05-27 17:20:44 +0900

    Add/Feat: 푸터 컴포넌트 구현

---
+++ src/components/common/TheFooter.vue
@@ -0,0 +1,18 @@
+<template>
+    <footer class="w-full h-[136px] bg-[#664021] flex items-center justify-center">
+        <div class="max-w-[1100px] w-full flex justify-center items-center px-5">
+            <div class="w-full flex justify-between items-center">
+                <img src="/images/footer_logo.svg" alt="" />
+                <div class="text-right text-[white] text-16 font-bold">
+                    버그 문의:<br />
+                    010-9070-4487
+                </div>
+            </div>
+            <span class="text-[#ffffff9c] text-[13px] absolute">copyright &copy; choisubin</span>
+        </div>
+    </footer>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped></style>
