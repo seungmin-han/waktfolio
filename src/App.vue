b23a4222031238ef6cfa3791df20cb6f49e02a4d
SEUNGMIN HAN <seungmin_han@kakao.com>
2024-05-27 17:20:12 +0900

    Update: 푸터 컴포넌트 추가

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,11 @@
 <template>
     <TheHeader />
     <RouterView />
+    <TheFooter />
 </template>
 
 <script setup>
-import { TheHeader } from '@/components/common';
+import { TheHeader, TheFooter } from '@/components/common';
 </script>
 
 <style scoped></style>
