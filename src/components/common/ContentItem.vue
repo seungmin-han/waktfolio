98eb852016d8dcda7d127634b5992d39df07e78c
SEUNGMIN HAN <seungmin_han@kakao.com>
2024-06-04 13:42:13 +0900

    Add/Feat: 게시글 프리뷰 컴포넌트 분리 생성

---
+++ src/components/common/ContentItem.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="flex flex-col gap-y-3 mx-2">
+        <div class="w-[343px] h-[244px] rounded-xl bg-[black]"></div>
+        <div class="px-1.5 flex justify-between items-center">
+            <div class="flex flex-col gap-y-1">
+                <span class="text-16 font-semibold text-[#171719]">{{ item.title }}</span>
+                <div class="flex gap-x-1">
+                    <div class="w-4 h-4 rounded-full bg-[#D9D9D9]"></div>
+                    <span class="font-medium text-[13px] text-[rgba(55, 56, 60, 0.61)]">{{
+                        item.memberName
+                    }}</span>
+                </div>
+            </div>
+            <div class="flex items-center gap-x-4">
+                <button class="flex flex-col items-center">
+                    <img src="/images/view.svg" alt="" class="w-[30px] h-[30px]" />
+                    <span class="font-semibold text-[10px] text-[rgba(55, 56, 60, 0.61)]">{{
+                        item.views
+                    }}</span>
+                </button>
+                <button class="flex flex-col items-center">
+                    <img src="/images/like.svg" alt="" class="w-[30px] h-[30px]" />
+                    <span class="font-semibold text-[10px] text-[rgba(55, 56, 60, 0.61)]">{{
+                        item.likes
+                    }}</span>
+                </button>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script setup>
+defineProps(['item']);
+</script>
+
+<style lang="scss" scoped></style>
