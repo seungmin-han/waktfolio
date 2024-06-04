31395f0360b95fbbea7f8a5deefabff4a7109d08
SEUNGMIN HAN <seungmin_han@kakao.com>
2024-06-04 17:56:13 +0900

    Update: 검색바 스타일 수정 및 추천검색어 슬라이드 컴포넌트 적용

--- src/components/common/TheHeader.vue
+++ src/components/common/TheHeader.vue
@@ -6,27 +6,27 @@
             </RouterLink>
         </div>
         <div id="search-tab" class="ml-[83px] max-w-[400px] w-full flex flex-col gap-y-4">
-            <div class="w-full border-b-[3px] border-b-gray-900 relative">
+            <div class="w-full relative border-b-[3px] border-b-gray-900">
                 <input
                     type="text"
-                    class="max-w-[370px] w-full leading-[29px] mb-2 bg-[transparent]"
+                    class="w-full max-w-[350px] ml-2 pb-[5px] text-16 leading-[24px] bg-[transparent] text-[#1717199c] focus:text-[#171719] focus:font-semibold outline-none"
                 />
-                <button class="w-[30px] h-[30px] absolute top-[2px] right-0">
+                <button class="w-[30px] h-[30px] absolute bottom-[2px] right-[5px]">
                     <img src="/images/search.svg" alt="" />
                 </button>
             </div>
             <div class="text-gray-800 gap-x-4 flex items-center">
                 <span class="text-16 font-bold flex-shrink-0">추천검색어</span>
-                <div
-                    class="flex gap-x-4 flex-nowrap items-center text-14 relative recommand overflow-hidden"
-                >
-                    <div
-                        v-for="word in recommandList"
-                        :key="word"
-                        class="py-[5px] px-[15px] rounded-full bg-gray-300 flex-shrink-0"
-                    >
-                        #{{ word }}
-                    </div>
+                <div class="text-14 relative recommand overflow-hidden">
+                    <Flicking :options="options">
+                        <div
+                            v-for="word in recommandList"
+                            :key="word"
+                            class="py-[5px] px-[15px] rounded-full bg-gray-300 flex-shrink-0 font-medium mr-4"
+                        >
+                            #{{ word }}
+                        </div>
+                    </Flicking>
                 </div>
             </div>
         </div>
@@ -42,13 +42,21 @@
 </template>
 
 <script setup>
+import Flicking from '@egjs/vue3-flicking';
 import { ref } from 'vue';
 
 const recommandList = ref(['눕프핵', '건콘', '유니티', '언리얼']);
+
+const options = ref({
+    align: 'prev',
+    bound: true,
+    circular: false,
+});
 </script>
 
 <style lang="postcss" scoped>
 .recommand {
-    @apply before:content-[''] before:absolute before:right-0 before:w-[50px] before:h-full before:bg-gradient-to-l from-gray-200 to-[rgba(255,255,255,0)];
+    @apply before:content-[''] before:absolute before:left-0 before:w-[30px] before:h-full before:bg-gradient-to-r from-gray-200 to-[rgba(255,255,255,0)] before:top-0 before:z-30;
+    @apply after:content-[''] after:absolute after:right-0 after:w-[30px] after:h-full after:bg-gradient-to-l from-gray-200 to-[rgba(255,255,255,0)] after:top-0 after:z-30;
 }
 </style>
