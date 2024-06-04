7ce321ff5852d0911b502a98f29d502c1a77c626
SEUNGMIN HAN <seungmin_han@kakao.com>
2024-06-04 11:57:01 +0900

    Update: 슬라이드 컴포넌트 공통으로 이동 및 내부 아이템 slot 영역 추가

--- src/components/common/ContentSlide.vue
+++ src/components/common/ContentSlide.vue
@@ -26,39 +26,7 @@
         </div>
         <div class="flex items-center w-full">
             <Flicking :options="options" ref="$flk" @changed="onSlide" @move="onSlide">
-                <div class="flex flex-col gap-y-3 mx-2" v-for="item in list" :key="item.contentId">
-                    <div class="w-[343px] h-[244px] rounded-xl bg-[black]"></div>
-                    <div class="px-1.5 flex justify-between items-center">
-                        <div class="flex flex-col gap-y-1">
-                            <span class="text-16 font-semibold text-[#171719]">{{
-                                item.title
-                            }}</span>
-                            <div class="flex gap-x-1">
-                                <div class="w-4 h-4 rounded-full bg-[#D9D9D9]"></div>
-                                <span
-                                    class="font-medium text-[13px] text-[rgba(55, 56, 60, 0.61)]"
-                                    >{{ item.memberName }}</span
-                                >
-                            </div>
-                        </div>
-                        <div class="flex items-center gap-x-4">
-                            <button class="flex flex-col items-center">
-                                <img src="/images/view.svg" alt="" class="w-[30px] h-[30px]" />
-                                <span
-                                    class="font-semibold text-[10px] text-[rgba(55, 56, 60, 0.61)]"
-                                    >{{ item.views }}</span
-                                >
-                            </button>
-                            <button class="flex flex-col items-center">
-                                <img src="/images/like.svg" alt="" class="w-[30px] h-[30px]" />
-                                <span
-                                    class="font-semibold text-[10px] text-[rgba(55, 56, 60, 0.61)]"
-                                    >{{ item.likes }}</span
-                                >
-                            </button>
-                        </div>
-                    </div>
-                </div>
+                <slot name="content"> </slot>
             </Flicking>
         </div>
     </div>
@@ -68,7 +36,7 @@
 import Flicking from '@egjs/vue3-flicking';
 import { ref } from 'vue';
 
-const props = defineProps(['title', 'list']);
+const props = defineProps(['title']);
 
 const $flk = ref();
 const currentIndex = ref(0);
